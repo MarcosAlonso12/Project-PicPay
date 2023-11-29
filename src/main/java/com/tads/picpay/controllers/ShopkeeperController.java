7702c36a39797c7330a2194832c3da60d545531b
Marco Antonio <hehemarcos300@gmail.com>
2023-11-28 20:51:11 -0400

    Criando controller, service para shopkeeper.

--- src/main/java/com/tads/picpay/controllers/ShopkeeperController.java
+++ src/main/java/com/tads/picpay/controllers/ShopkeeperController.java
@@ -1,7 +1,23 @@
 package com.tads.picpay.controllers;
 
+import com.tads.picpay.dto.ShopkeeperDTO;
+import com.tads.picpay.services.ShopkeeperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/shopkeepers")
 public class ShopkeeperController {
+    @Autowired
+    private ShopkeeperService shopkeeperService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ShopkeeperDTO> findById(@PathVariable Long id) {
+        ShopkeeperDTO shopkeeperDTO = shopkeeperService.findById(id);
+        return ResponseEntity.ok(shopkeeperDTO);
+    }
 }
--- src/main/java/com/tads/picpay/services/ShopkeeperService.java
+++ src/main/java/com/tads/picpay/services/ShopkeeperService.java
@@ -1,7 +1,16 @@
 package com.tads.picpay.services;
 
+import com.tads.picpay.dto.ShopkeeperDTO;
+import com.tads.picpay.repositories.ShopkeeperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShopkeeperService {
+    @Autowired
+    private ShopkeeperRepository shopkeeperRepository;
+
+    public ShopkeeperDTO findById(Long id) {
+        return new ShopkeeperDTO(shopkeeperRepository.findById(id).orElseThrow(() -> new RuntimeException("Teste")));
+    }
 }
