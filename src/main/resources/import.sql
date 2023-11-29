04ef22263435e25d2c397772bc3bbe1a7e432860
Marco Antonio <hehemarcos300@gmail.com>
2023-11-28 20:23:39 -0400

    Populando banco de dados.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,13 @@
 insert into users (id, name, cpf, email, password, amount) values (1, 'Ana', '123.456.789-01', 'ana@example.com', 'ana123', 1000.00);
 insert into users (id, name, cpf, email, password, amount) values (2, 'Bruno', '123.456.789-02', 'bruno@example.com', 'bruno123', 1500.00);
+insert into users (id, name, cpf, email, password, amount) values (3, 'Carlos', '123.456.789-01', 'carlos@example.com', 'carlos123', 3000.39);
+insert into users (id, name, cpf, email, password, amount) values (4, 'Donald', '123.456.789-02', 'donald@example.com', 'donald123', 500.50);
+insert into users (id, name, cpf, email, password, amount) values (5, 'Edinaldo', '123.456.789-01', 'edinaldo@example.com', 'edinaldo123', 1000.39);
+insert into users (id, name, cpf, email, password, amount) values (6, 'Flávia', '123.456.789-02', 'flavia@example.com', 'flavia123', 6000.50);
+
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (1, 'Ana Comércios', '12.345.671/0001-00', 'ana.comercio@example.com', 'anac123', 10000.35);
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (2, 'Bruno AutoPeças', '12.345.672/0001-00', 'b.autopecas@example.com', 'bruno123', 50000.20);
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (3, 'Mercado Bom Jesus', '12.345.673/0001-00', 'merc.carlos@example.com', 'carlos123', 100000.00);
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (4, 'Banco Santos', '12.345.674/0001-00', 'b.santos@example.com', 'santos.b123', 5000000.30);
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (5, 'Assistencia Souza', '12.345.675/0001-00', 's.ass@example.com', 'souza123', 30000.00);
+insert into shopkeepers (id, name, cnpj, email, password, amount) values (6, 'Borracharia Tours', '12.345.676/0001-00', 'tours@example.com', 'tours123', 1000.30);
