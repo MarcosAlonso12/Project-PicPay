65b236da49324009e422ca2d5fc5ef7f2dd62e85
Marco Antonio <hehemarcos300@gmail.com>
2023-12-02 14:57:37 -0400

    CRUD Shopkeeper.

--- src/main/java/com/tads/picpay/controllers/ShopkeeperController.java
+++ src/main/java/com/tads/picpay/controllers/ShopkeeperController.java
@@ -3,11 +3,13 @@ package com.tads.picpay.controllers;
 import com.tads.picpay.dto.ShopkeeperDTO;
 import com.tads.picpay.services.ShopkeeperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/shopkeepers")
@@ -20,4 +22,30 @@ public class ShopkeeperController {
         ShopkeeperDTO shopkeeperDTO = shopkeeperService.findById(id);
         return ResponseEntity.ok(shopkeeperDTO);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ShopkeeperDTO>> findAll(Pageable pageable) {
+        Page<ShopkeeperDTO> shopkeeperDTOS = shopkeeperService.findAll(pageable);
+        return ResponseEntity.ok(shopkeeperDTOS);
+    }
+
+    @PostMapping
+    public ResponseEntity<ShopkeeperDTO> insert(@RequestBody ShopkeeperDTO shopkeeperDTO) {
+        shopkeeperDTO = shopkeeperService.insert(shopkeeperDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(shopkeeperDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(shopkeeperDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ShopkeeperDTO> update(@PathVariable Long id, @RequestBody ShopkeeperDTO shopkeeperDTO) {
+        shopkeeperDTO = shopkeeperService.update(id, shopkeeperDTO);
+        return ResponseEntity.ok(shopkeeperDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        shopkeeperService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/tads/picpay/services/ShopkeeperService.java
+++ src/main/java/com/tads/picpay/services/ShopkeeperService.java
@@ -1,9 +1,13 @@
 package com.tads.picpay.services;
 
 import com.tads.picpay.dto.ShopkeeperDTO;
+import com.tads.picpay.entities.Shopkeeper;
 import com.tads.picpay.repositories.ShopkeeperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ShopkeeperService {
@@ -13,4 +17,49 @@ public class ShopkeeperService {
     public ShopkeeperDTO findById(Long id) {
         return new ShopkeeperDTO(shopkeeperRepository.findById(id).orElseThrow(() -> new RuntimeException("Teste")));
     }
+
+    @Transactional(readOnly = true)
+    public Page<ShopkeeperDTO> findAll(Pageable pageable) {
+        return shopkeeperRepository.findAll(pageable).map(ShopkeeperDTO::new);
+    }
+
+    @Transactional
+    public ShopkeeperDTO insert(ShopkeeperDTO shopkeeperDTO) {
+        Shopkeeper shopkeeper = new Shopkeeper(
+                shopkeeperDTO.getName(),
+                shopkeeperDTO.getEmail(),
+                shopkeeperDTO.getPassword(),
+                shopkeeperDTO.getAmount(),
+                shopkeeperDTO.getType(),
+                shopkeeperDTO.getCnpj()
+        );
+        shopkeeper = shopkeeperRepository.save(shopkeeper);
+        return new ShopkeeperDTO(shopkeeper);
+    }
+
+    @Transactional
+    public ShopkeeperDTO update(Long id, ShopkeeperDTO shopkeeperDTO) {
+        try {
+            Shopkeeper shopkeeper = shopkeeperRepository.getReferenceById(id);
+            shopkeeper.setName(shopkeeperDTO.getName());
+            shopkeeper.setEmail(shopkeeperDTO.getEmail());
+            shopkeeper.setPassword(shopkeeperDTO.getPassword());
+            shopkeeper.setAmount(shopkeeperDTO.getAmount());
+            shopkeeper.setType(shopkeeperDTO.getType());
+            shopkeeper.setCnpj(shopkeeperDTO.getCnpj());
+            return new ShopkeeperDTO(shopkeeper);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        try {
+            shopkeeperRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
 }
